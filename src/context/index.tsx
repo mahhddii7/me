dbb4feba71ea44c0d371d74f5f423d7158460a89
mahhddii7 <mahhddii7@gmail.com>
2025-02-15 21:55:40 +0330

    push

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import React, {createContext, useRef} from "react";
+import  {createContext, useRef} from "react";
 
 
 export const contextApi = createContext({} as any);
